523c71443d4a9a22b93023db8b6e76d3e25e1fe7
robertnasha <bksnvnjf308@outlook.com>
2025-12-24 17:02:34 +0800

    Wire VeilFileVault into deploy script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedVault = await deploy("VeilFileVault", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`VeilFileVault contract: `, deployedVault.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_veilFileVault"; // id required to prevent reexecution
+func.tags = ["VeilFileVault"];
